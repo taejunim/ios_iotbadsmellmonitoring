379eb6124e964d9389539703e8286f34c5883025
Ki-jeong Kang <dev.kjkang@gmail.com>
2021-05-27 13:20:59 +0900

    Initial commit

---
+++ IoTBadSmellMonitoring/IoTBadSmellMonitoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  IoTBadSmellMonitoringApp.swift
+//  IoTBadSmellMonitoring
+//
+//  Created by KJ on 2021/05/27.
+//
+
+import SwiftUI
+
+@main
+struct IoTBadSmellMonitoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IoTBadSmellMonitoring/Sources/View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IoTBadSmellMonitoring
+//
+//  Created by KJ on 2021/05/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
